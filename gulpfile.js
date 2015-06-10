2bb86ed2a693004670bed633b6ac598a95271bc5
Gabriel Pedro <gpedro831@gmail.com>
2015-06-10 17:50:58 -0400

    Iniciando migração
    Mudamos o workflow para Stylus, Jeet e Rupture com Nunjucks para Templating.

---
+++ gulpfile.js
@@ -0,0 +1,125 @@
+/*global -$ */
+'use strict';
+// generated on 2015-06-10 using generator-gulp-webapp 0.3.0
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var jeet = require('jeet');
+var rupture = require('rupture');
+
+gulp.task('styles', function () {
+  return gulp.src('app/styles/application.styl')
+    .pipe($.sourcemaps.init())
+    .pipe($.stylus({
+        'include css': true,
+        use: [jeet(), rupture()]
+    }).on('error', $.util.log))
+    .pipe($.postcss([
+      require('autoprefixer-core')()
+    ]))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('.tmp/styles'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('jshint', function () {
+  return gulp.src('app/scripts/**/*.js')
+    .pipe(reload({stream: true, once: true}))
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
+});
+
+gulp.task('html', ['views', 'styles'], function () {
+  var assets = $.useref.assets({searchPath: ['.tmp', 'app', '.']});
+
+  return gulp.src(['app/*.html', '.tmp/*.html'])
+    .pipe(assets)
+    .pipe($.if('*.js', $.uglify()))
+    .pipe($.if('*.css', $.csso()))
+    .pipe(assets.restore())
+    .pipe($.useref())
+    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', function () {
+  return gulp.src('app/images/**/*')
+    .pipe($.cache($.imagemin({
+      progressive: true,
+      interlaced: true,
+      // don't remove IDs from SVGs, they are often used
+      // as hooks for embedding and styling
+      svgoPlugins: [{cleanupIDs: false}]
+    })))
+    .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('views', function () {
+  $.nunjucksRender.nunjucks.configure(['app/']);
+
+  return gulp.src('app/*.html')
+    .pipe($.nunjucksRender())
+    .pipe(gulp.dest('.tmp'))
+});
+
+gulp.task('extras', function () {
+  return gulp.src([
+    'app/*.*',
+    '!app/*.html'
+  ], {
+    dot: true
+  }).pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
+
+gulp.task('serve', ['views', 'styles'], function () {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['.tmp', 'app'],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
+
+  // watch for changes
+  gulp.watch([
+    '.tmp/*.html',
+    'app/scripts/**/*.js',
+    'app/images/**/*'
+  ]).on('change', reload);
+
+  gulp.watch('app/**/*.html', ['views', reload]);
+  gulp.watch('app/styles/**/*.scss', ['styles']);
+  gulp.watch('bower.json', ['wiredep']);
+});
+
+// inject bower components
+gulp.task('wiredep', function () {
+  var wiredep = require('wiredep').stream;
+
+  gulp.src('app/styles/*.scss')
+    .pipe(wiredep({
+      ignorePath: /^(\.\.\/)+/
+    }))
+    .pipe(gulp.dest('app/styles'));
+
+  gulp.src('app/layouts/*.html')
+    .pipe(wiredep({
+      ignorePath: /^(\.\.\/)*\.\./
+    }))
+    .pipe(gulp.dest('app/layouts'));
+});
+
+gulp.task('build', ['jshint', 'html', 'images', 'extras'], function () {
+  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+});
+
+gulp.task('default', ['clean'], function () {
+  gulp.start('build');
+});
